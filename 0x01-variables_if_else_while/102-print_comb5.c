02f55f22b7308ba5d23aa90c08008bf34c169254
Dcardinal042 <samokoh40@gmail.com>
2023-02-18 11:53:57 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,43 +1,48 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible combinations of two two-digit numbers
+ * main - Prints numbers between 00 to 99.
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-int i, j, k, l;
+int i, e, g, h, op1, op2;
 
-for (i = 0; i <= 9; i++)
+i = e = g = h = 48;
+while (h < 58)
 {
-for (j = 0; j <= 9; j++)
-{
-for (k = 0; k <= 9; k++)
-{
-for (l = 0; l <= 9; l++)
-{
-if ((i < k) || ((i == k) && (j < l)))
-{
-putchar(i + '0');
-putchar(j + '0');
-putchar(' ');
-putchar(k + '0');
-putchar(l + '0');
-
-if (i != 9 || j != 8 || k != 9 || l != 9)
-{
-putchar(',');
-putchar(' ');
+	g = 48;
+	while (g < 58)
+	{
+		e = 48;
+		while (e < 58)
+		{
+			i = 48;
+			while (i < 58)
+			{
+				op1 = (h * 10) + g;
+				op2 = (e * 10) + i;
+				if (op1 < op2)
+				{
+					putchar(h);
+					putchar(g);
+					putchar(' ');
+					putchar(e);
+					putchar(i);
+					if (h == 57 && g == 56 && e == 57 && i == 57)
+						break;
+					putchar(',');
+					putchar(' ');
+				}
+				i++;
+			}
+			e++;
+		}
+		g++;
+	}
+	h++;
 }
-}
-}
-}
-}
-}
-
 putchar('\n');
-
-return (0);
+return (0);
 }
